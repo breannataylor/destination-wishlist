7d9bbc3c8dedcf69505f609a8e44659db46bbd1b
BreAnna Taylor <thebreannataylor@gmail.com>
2023-12-14 14:13:55 -0800

    created card and content

--- script.js
+++ script.js
@@ -7,10 +7,48 @@ user_input_form.addEventListener("submit", (e) => {
     //variables to hold user input
     const destinationName = destination_name.value;
     const locationName = destination_location.value;
-    const photoURL = photo_url === "" ? "https://picsum.photos/200/300" : photo_url.value
+    const photoURL = photo_url === "" ? "https://picsum.photos/200/300" : photo_url.value;
     //another way to write a ternary statement:
     // const photoURL = photo_url.value || "https://picsum.photos/200/300";
     const desc = destination_name.value;
 
-    console.log(destinationName, locationName, photoURL, desc);
-})
+    //clear form
+    user_input_form.reset();
+
+    //create card
+    const card = createCard({destinationName, locationName, photoURL, desc});
+
+    //append card to card container
+    card_container.appendChild(card);
+});
+
+function createCard({destinationName, locationName, photoURL, desc}) {
+    /* Bootstrap card html
+ <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Location</p>
+    <p class="card-text">Desc</p>
+    <button class="btn btn-info">Edit</button>
+    <button class="btn btn-danger">Delete</button>
+  </div>
+</div> */
+
+    //div for card
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.setAttribute("style", "width: 18rem;");
+    //html for cardbody
+    card.innerHTML = `
+    <img class="card-img-top" src="${photoURL}" alt="${destinationName} at ${locationName}">
+    <div class="card-body">
+    <h5 class="card-title">${destinationName}</h5>
+    <p class="card-text">${locationName}</p>
+    ${desc && `<p class="card-text">${desc}</p>`}
+    <button class="btn btn-info">Edit</button>
+    <button class="btn btn-danger">Delete</button>
+    `;
+
+    return card;
+}
